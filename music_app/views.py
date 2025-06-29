77b41378d7941940d777e7b5aeb1a4468a78665b
Pipusk <klimentfeofilov@gmail.com>
2025-06-29 20:12:47 +0300

    new

--- music_app/views.py
+++ music_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Album
 
 def home_view(request):
-    albums = Album.objects.prefetch_related('tracks').all()
+    albums = Album.objects.all()
     return render(request, 'music_app/home.html', {'albums': albums})
 
 

34ee992422011d4cdf6e2f12076d3657a1d4a83a
Pipusk <klimentfeofilov@gmail.com>
2025-06-29 18:23:25 +0300

    Configure static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "music_app" / "static",  # если используется локальная структура
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

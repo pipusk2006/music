07beaddea175faf0db44c91bade67c20e6192cd9
Pipusk <klimentfeofilov@gmail.com>
2025-06-29 21:02:18 +0300

    new

--- music_app/static/music_app/js/expand.js
+++ music_app/static/music_app/js/expand.js
@@ -4,22 +4,26 @@ function toggleAlbum(index) {
 
   allCards.forEach((card, i) => {
     const isCurrent = i === index;
+    const isAlreadyExpanded = card.classList.contains('expanded');
 
-    if (isCurrent) {
+    if (isCurrent && !isAlreadyExpanded) {
+      card.classList.remove('compact');
       card.classList.add('expanded');
       allExpanded[i].classList.remove('hidden');
     } else {
       card.classList.remove('expanded');
+      card.classList.add('compact');
       allExpanded[i].classList.add('hidden');
     }
   });
 
-  // Прокрутка к активному альбому
+  // Прокрутка к текущей карточке (если она развёрнута)
   const currentCard = allCards[index];
-  if (currentCard) {
+  if (currentCard.classList.contains('expanded')) {
     currentCard.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
+
 
 
 

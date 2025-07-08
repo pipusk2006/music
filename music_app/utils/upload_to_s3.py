66e0291fd1613ceddbcd21e191bd253ef629383d
Pipusk <klimentfeofilov@gmail.com>
2025-07-08 16:28:17 +0300

    new

--- music_app/utils/upload_to_s3.py
+++ music_app/utils/upload_to_s3.py
@@ -1,23 +1,31 @@
-# utils/upload_to_s3.py
 import boto3
 from django.conf import settings
-import urllib.parse
-
+from botocore.exceptions import ClientError
 
 def upload_to_s3(file_obj, filename):
-    filename = filename.replace(" ", "_")  # normalize
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='s3',
-        endpoint_url=settings.S3_ENDPOINT_URL,
-        aws_access_key_id=settings.S3_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.S3_SECRET_ACCESS_KEY,
-    )
+    try:
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='s3',
+            endpoint_url=settings.S3_ENDPOINT_URL,
+            aws_access_key_id=settings.S3_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.S3_SECRET_ACCESS_KEY,
+        )
 
-    client.upload_fileobj(file_obj, settings.S3_BUCKET_NAME, filename)
+        content_type = 'audio/mpeg' if filename.endswith('.mp3') else 'image/jpeg'
 
-    # Make sure filename is URL-safe (e.g., escaping spaces etc.)
-    encoded_filename = urllib.parse.quote(filename)
+        client.upload_fileobj(
+            file_obj,
+            settings.S3_BUCKET_NAME,
+            filename,
+            ExtraArgs={'ContentType': content_type}
+        )
 
-    return f"{settings.S3_PUBLIC_URL_PREFIX}/{encoded_filename}"
+        print(f"✅ Uploaded to: {filename}")
+        return f"{settings.S3_PUBLIC_URL_PREFIX}/{filename}"
+    
+    except ClientError as e:
+        print(f"❌ Ошибка при загрузке {filename}: {e}")
+        return None
+
 

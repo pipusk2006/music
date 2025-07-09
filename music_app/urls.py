0130496877b954f031c1b964c2dbcf8cc0ee19e3
Pipusk <klimentfeofilov@gmail.com>
2025-07-09 11:50:55 +0300

    new

--- music_app/urls.py
+++ music_app/urls.py
@@ -5,6 +5,8 @@ from .views import (
     toggle_favorite, upload_album_view
 )
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -17,5 +19,6 @@ urlpatterns = [
     path('toggle_favorite/', toggle_favorite, name='toggle_favorite'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('upload/', upload_album_view, name='upload_album'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
